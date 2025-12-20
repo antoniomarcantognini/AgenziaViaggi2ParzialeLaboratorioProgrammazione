1078200f0c710aa00e71aa2781966a2537221604
antoniomarcantognini <antoni.marcantognini@studenti.unicam.it>
2025-12-20 10:44:05 +0100

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,34 +1,38 @@
 #pragma once
-#include "Tipologia_cliente.h"
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <memory>
+#include "Tipologia_cliente.h"
 
 class Cliente {
-
-private: 
+private:
     std::string codice_cliente; // Formato: "CLT-XXXX"
     std::string nome;
     std::string cognome;
     std::string email;
     std::string telefono;
     int eta;
-    Tipologia_cliente tipologia_cliente; // Standard, Premium o VIP
+    Tipologia_cliente tipologia;
+
+    // Costruttore privato: può essere chiamato solo dal metodo statico crea_cliente
+    Cliente(std::string codice, std::string nome, std::string cognome, 
+            std::string email, std::string tel, int eta, Tipologia_cliente tipo);
 
 public:
-    Cliente(std::string codice,std::string nome, std::string cognome, std::string email, std::string tel, int eta, Tipologia_cliente tipo);
+    static std::shared_ptr<Cliente> crea_cliente(std::string codice, std::string nome, std::string cognome, 
+                                                 std::string email, std::string tel, int eta, Tipologia_cliente tipo);
 
     // Getter
     std::string get_codice_cliente() const;
-    std::string get_nome_completo() const; // Ritorna "Nome Cognome"
+    std::string get_nome_completo() const;  // Ritorna "Nome Cognome"
     int get_eta() const;
     Tipologia_cliente get_tipologia_cliente() const;
-    
-    double applica_sconto(double prezzo_base) const;
-    // Standard: 0%, Premium: 10%, VIP 20%
 
+    // Logica di business
+    double applica_sconto(double prezzo_base) const; // Standard: 0%, Premium: 10%, VIP 20%
     std::string stampa_info() const;
 
-    // Metodi statici di supporto per enum
+    // Helpers statici
     static std::string tipologia_to_string(Tipologia_cliente tipo);
     static Tipologia_cliente string_to_tipologia(std::string tipo);
 };
