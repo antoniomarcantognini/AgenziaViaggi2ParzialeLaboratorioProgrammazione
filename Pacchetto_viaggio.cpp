5f0af98391294eae148c34a6e0f3a8d5f79362fa
antoniomarcantognini <antoni.marcantognini@studenti.unicam.it>
2025-12-28 16:22:22 +0100

    Update Pacchetto_viaggio.cpp

--- Pacchetto_viaggio.cpp
+++ Pacchetto_viaggio.cpp
@@ -1,29 +1,29 @@
 #include "Pacchetto_viaggio.h"
 #include <iostream>
 #include <stdexcept>
+#include <fstream>
 
 using namespace std;
 
-// Costruttore:
+// Costruttore
 Pacchetto_viaggio::Pacchetto_viaggio(string codice, string dest, int giorni, double prezzo)
     : codice_pacchetto(codice), destinazione(dest), durata_giorni(giorni), prezzo_base(prezzo)
 {
-    // Validazione input:
-    if(giorni <= 0) throw invalid_argument("Durata del viaggio negativa.");
-    if(prezzo < 0) throw invalid_argument("Prezzo base negativo.");
-    if(codice.empty()) throw invalid_argument("Codice vuoto.");
-    if(dest.empty()) throw invalid_argument("Destinazione vuota.");
+    // Validazione input base
+    if(giorni <= 0) throw runtime_error("Durata del viaggio negativa.");
+    if(prezzo < 0) throw runtime_error("Prezzo base negativo.");
+    if(codice.empty()) throw runtime_error("Codice vuoto.");
+    if(dest.empty()) throw runtime_error("Destinazione vuota.");
 
     this->disponibile = true;
     cout << "Costruito pacchetto base: " << codice << endl;
 }
 
-// Distruttore virtuale:
 Pacchetto_viaggio::~Pacchetto_viaggio(){
     cout << "Distruzione pacchetto base: " << codice_pacchetto << endl;
 }
 
-// Getter:
+// Getter
 string Pacchetto_viaggio::get_codice_pacchetto() const{
     return this->codice_pacchetto;
 }
@@ -48,11 +48,13 @@ void Pacchetto_viaggio::set_disponibile(bool disponibilita){
     this->disponibile = disponibilita;
 }
 
-bool Pacchetto_viaggio::salva_dati_su_file(ofstream& file){
+// Implementazione salvataggio base
+bool Pacchetto_viaggio::salva_dati_su_file(ofstream& file) const {
+    // Nota: non mettiamo endl qui perché le classi derivate devono aggiungere roba sulla stessa riga
     file << this->codice_pacchetto << ";"
-         << this->destinazione << ";";
+         << this->destinazione << ";"
          << this->durata_giorni << ";"
-         << (this->disponibile ? "Disponibile" : "Non Disponibile") << ";";
+         << (this->disponibile ? "Disponibile" : "Non Disponibile") << ";"
          << this->prezzo_base << ";";
     return true;
 }
